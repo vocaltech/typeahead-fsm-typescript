a9bef60283548554418315e3b11976c22886a5cd
vocaltech <it@vocaltech.fr>
2022-03-12 10:20:48 +0100

    Update highlighted_suggestion_changed and states

--- src/typeahead-fsm.ts
+++ src/typeahead-fsm.ts
@@ -1,3 +1,5 @@
+import { depts_villes } from './models/depts_villes'
+
 // states
 export enum TypeaheadStates {
     INIT = 'INIT',
@@ -14,7 +16,8 @@ export enum TypeaheadEvents {
     selection_entered = 'selection_entered',
     selection_cleared = 'selection_cleared',
     text_changed = 'text_changed',
-    highlighted_suggestion_changed = 'highlighted_suggestion_changed'
+    move_highlighted_suggestion_up = 'move_highlighted_suggestion_up',
+    move_highlighted_suggestion_down = 'move_highlighted_suggestion_down'
 }
 
 // fsm map
@@ -25,16 +28,25 @@ export const TypeaheadFsmMap = {
             [TypeaheadEvents.data_loaded]: TypeaheadStates.READY_FOR_INPUT
         },
         [TypeaheadStates.READY_FOR_INPUT]: {
-            [TypeaheadEvents.got_input_focus]: TypeaheadStates.SUGGESTIONS_VISIBLE
+            [TypeaheadEvents.got_input_focus]: TypeaheadStates.SUGGESTIONS_VISIBLE,
+            depts_villes,
+            text: ''
         },
         [TypeaheadStates.SUGGESTIONS_VISIBLE]: {
             [TypeaheadEvents.lost_input_focus]: TypeaheadStates.READY_FOR_INPUT,
             [TypeaheadEvents.selection_entered]: TypeaheadStates.SELECTED,
             [TypeaheadEvents.text_changed]: TypeaheadStates.SUGGESTIONS_VISIBLE,
-            [TypeaheadEvents.highlighted_suggestion_changed]: TypeaheadStates.SUGGESTIONS_VISIBLE
+            [TypeaheadEvents.move_highlighted_suggestion_down]: TypeaheadStates.SUGGESTIONS_VISIBLE,
+            [TypeaheadEvents.move_highlighted_suggestion_up]: TypeaheadStates.SUGGESTIONS_VISIBLE,
+            depts_villes,
+            text: '',
+            suggestions: [''],
+            suggestionIdx: 0
         },
         [TypeaheadStates.SELECTED]: {
-            [TypeaheadEvents.selection_cleared]: TypeaheadStates.READY_FOR_INPUT
+            [TypeaheadEvents.selection_cleared]: TypeaheadStates.READY_FOR_INPUT,
+            depts_villes,
+            selection: ''
         }
     }
 }
